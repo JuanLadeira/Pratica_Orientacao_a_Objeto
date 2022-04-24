1fb70e43e056f6990095f369882316bb2ff0a1db
JuanLadeira <juan.fll@hotmail.com>
2022-04-23 23:56:34 -0300

    Tratamento de condições para funcionamento do Sistema de conta corrente.py

--- OO/Sistema de conta corrente.py
+++ OO/Sistema de conta corrente.py
@@ -7,18 +7,27 @@ class ContaCorrente:  # CRIAÇÃO DA CLASSE CONTA CORRENTE
 
     def consultar_saldo(self):  # METODO PARA CONSULTAR SALDO
         print(f'O saldo atual da conta inscrita sob o CPF:{self.cpf} é de R${self.saldo:,.2f}')
-        return self.saldo
+        return f'o seu saldo atual é de R${self.saldo:,.2f}'
 
     def depositar(self, valor):  # METODO PARA DEPOSITAR DINHEIRO
         self.saldo += valor
 
     def sacar(self, valor):  # METODO PARA SACAR DINHEIRO
-        self.saldo -= valor
+        if self.saldo <= 0:
+            print(f'Saldo insuficiente, {self.consultar_saldo()}')
+        else:
+            self.saldo -= valor
 
-    def transferencia(self, valor, outra_conta):  # METODO PARA TRANSFERIR DINHEIRO PARA OUTRA CONTA
-        self.saldo -= valor
-        outra_conta.saldo = valor
-        print(f'O valor de R${valor:,.2f} foi transferido com sucesso!!')
+    def transferencia(self, valor, outra_conta):  # METODO PARA TRANSFERIR DINHEIRO PARA OUTRA CONTA.
+        # CASO O SALDO DA CONTA SEJA INFERIOR AO VALOR DE TRANSFERÊNCIA A TRANSAÇÃO SERÁ RECUSADA.
+        if self.saldo - valor <= 0:
+            print(f'Saldo insuficiente para concluir a operação, {self.consultar_saldo()} e o valor que deseja '
+                  f'transferir é de R${valor:,.2f}')
+        else:
+            self.saldo -= valor
+            outra_conta.saldo = valor
+            print(f'O valor de R${valor:,.2f} foi transferido com sucesso!!')
+
 
 
 if __name__ == '__main__':
@@ -27,5 +36,5 @@ if __name__ == '__main__':
     conta.sacar(50)
     conta.consultar_saldo()
     conta2 = ContaCorrente('Fernando', '132.231.444-56')
-    conta.transferencia(35, conta2)
+    conta.transferencia(200, conta2)
     conta2.consultar_saldo()
