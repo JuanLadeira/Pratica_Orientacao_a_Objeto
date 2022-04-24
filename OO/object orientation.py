6917e90ee0b2fb3001540c9d32a4de4014e4df4c
JuanLadeira <juan.fll@hotmail.com>
2022-04-23 22:57:23 -0300

    Exercicio do Objeto TV implementado, com suas respectivas descrições!

--- OO/object orientation.py
+++ OO/object orientation.py
@@ -1,34 +1,33 @@
-import self as self
+class TV:  # OBJETO TV
+    COR = 'Preta'   # ATRIBUTO DE CLASSE, OU SEJA, CASO FOR ALTERADO, ALTERARÁ TODOS OS DEMAIS
+    # OBJETOS QUE UTILIZAREM ESSA CLASE.
 
-nome = 'Felipe'
-
-print(type(nome))
-
-print(nome.capitalize())
-
-
-class TV:
-
-    def __init__(self):
-        self.cor = 'preta'
+    def __init__(self):  # ATRIBUTOS DO OBJETO TV
         self.tamanho = 50
         self.canal = 'Netflix'
         self.volume = 60
         self.status = False
 
-    def mudar_de_canal(self, canal):
-        self.canal = canal
+    def ligar_tv(self):
+        self.status = True
 
-    def aumentar_volume(self, volume=1):
+    def mudar_de_canal(self, novo_canal):  # METODO DE TROCAR DE CANAL DO OBJETO TV
+        self.canal = novo_canal
+        print(f'Canal da Tv alterado para {novo_canal}.')
+
+    def aumentar_volume(self, volume=1):  # METODO DE AUMENTAR O VOLUME DO OBJETO TV
         self.volume += volume
+        print(f'Volume da TV aumentado para {self.volume}.')
 
-    def diminuir_volume(self, volume=1):
+    def diminuir_volume(self, volume=1):  # METODO DE DIMINUIR VOLUME DO OBJETO TV
         self.volume -= volume
+        print(f'Volume reduzido para {self.volume}.')
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # TESTES DE FUNCIONAMENTO
     tv_sala = TV()
     tv_sala.diminuir_volume(10)
     tv_quarto = TV()
+    tv_quarto.mudar_de_canal('Youtube')
     print(tv_sala.volume)
     print(tv_quarto.volume)
