6f193fec4bd7c266363545d4a481c7259b498558
Juan Felipe <103801874+JuanLadeira@users.noreply.github.com>
2023-03-01 13:01:42 -0300

    alteração da inserção dos resultados com metodo adicionar pontos e inclusão do metodo calcular_incerteza_da_repetitividade, alteração dos metodos calcular_media e calcular_desvio_padrão para ficar coerente com a nova inserção dos resultados

--- OO/cali.py
+++ OO/cali.py
@@ -1,21 +1,45 @@
-class Incertezas():
-    def __init__(self, lista):
-        self.medicoes = lista
+import math
+
+class Instrumento():
+    def __init__(self):
+        self.medicoes_por_ponto = []
     
+    def adicionar_medicao(self, ponto, medicao):
+        if len(self.medicoes_por_ponto) < ponto:
+            self.medicoes_por_ponto.append([medicao])
+        else:
+            self.medicoes_por_ponto[ponto-1].append(medicao)
+
+    def calcular_media(self, ponto):
+        medicoes = self.medicoes_por_ponto[ponto-1]
+        return sum(medicoes)/ len(medicoes)
+
+    def calcular_desvio_padrao(self, ponto):
+        medicoes = self.medicoes_por_ponto[ponto-1]
+        media = self.calcular_media(ponto)
+        return math.sqrt(sum([(medicao - media)**2 for medicao in medicoes])/ (len(medicoes)-1))
     
-    def calcular_media(self):
-        soma = 0
-        for medicao in self.medicoes:
-            soma += medicao
-        média = soma/len(self.medicoes)
-        return média
+    def calcular_incerteza_da_repetitividade(self, ponto):
+        medicoes = self.medicoes_por_ponto[ponto-1]
+        desvio_padrao = self.calcular_desvio_padrao(ponto)
+        return desvio_padrao/math.sqrt(len(medicoes))
+    
+    def exibir_resultados(self):
+        for i, medicoes in enumerate(self.medicoes_por_ponto):
+            media = self.calcular_media(i+1)
+            desvio_padrao = self.calcular_desvio_padrao(i+1)
+            incerteza_tipo_A = self.calcular_incerteza_da_repetitividade(i+1)
+            print(f'Ponto {i+1}: média = {media:.2f}, desvio padrão = {desvio_padrao:.2f}, Ua = {incerteza_tipo_A:.2f}')
+
     
 
 
 
 if __name__ == "__main__":
-    medicoes = [10, 10 , 10, 10]
-    incerteza = Incertezas(medicoes)
-    média = incerteza.calcular_media()
+    instrumento = Instrumento()
+    instrumento.adicionar_medicao(1, 19)
+    instrumento.adicionar_medicao(1, 20)
+    instrumento.adicionar_medicao(1, 20)
+    instrumento.exibir_resultados()
 
-    print(média)
+
